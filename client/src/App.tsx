1853ce8cd1cef56ce7440b84ecab29b205e2b0f3
Michał Wójcik <michal.wojcik21@gmail.com>
2022-10-06 20:43:31 +0200

    Update App.tsx with new components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,30 @@
 import React from "react";
 import "./App.css";
+import Nav from "./components/nav/Nav";
+import StandardCalculator from "./components/Pages/StandardCalculator/StandardCalculator";
+import BmiCalculator from "./components/Pages/BmiCalculator/BmiCalculator";
 
 function App() {
-  return <div className="App">Hi</div>;
+  let calculatorComponent: JSX.Element;
+
+  switch (window.location.pathname) {
+    case "/":
+      calculatorComponent = <StandardCalculator />;
+      break;
+    case "/bmi":
+      calculatorComponent = <BmiCalculator />;
+      break;
+    default:
+      calculatorComponent = <StandardCalculator />;
+      break;
+  }
+
+  return (
+    <div className="App">
+      <Nav />
+      {calculatorComponent}
+    </div>
+  );
 }
 
 export default App;
