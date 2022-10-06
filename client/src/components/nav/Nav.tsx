dd9c89b3ef74f7937de4b1c93188b1aaf6094f7f
Michał Wójcik <michal.wojcik21@gmail.com>
2022-10-06 20:42:21 +0200

    Create Navigation Bar with basic css

---
+++ client/src/components/nav/Nav.tsx
@@ -0,0 +1,33 @@
+﻿import React from "react";
+import "./Nav.css";
+
+const Nav = () => {
+  return (
+    <nav className="nav">
+      <ul>
+        <LinkElement href="/">Standard</LinkElement>
+        <LinkElement href="/bmi">BMI</LinkElement>
+      </ul>
+    </nav>
+  );
+};
+
+const LinkElement: React.FC<LinkElementProps> = ({ href, children, ...props }): JSX.Element => {
+  const pathName = window.location.pathname;
+
+  return (
+    <li className={pathName === href ? "active" : ""}>
+      <a href={href} {...props}>
+        {children}
+      </a>
+    </li>
+  );
+};
+
+interface LinkElementProps {
+  href: `/${string}`;
+  children: any;
+  [x: string]: any;
+}
+
+export default Nav;
